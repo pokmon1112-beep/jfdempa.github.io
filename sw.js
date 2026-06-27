046d1ce3dfc1d933383d1316326660a541e77659
pokmon1112-beep <pokmon1112@gmail.com>
2026-06-28 00:56:15 +0800

    Add files via upload
    1

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'pkmn-calc-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 安裝時快取核心檔案
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 攔截網路請求，優先使用快取
+self.addEventListener('fetch', (event) => {
+  // 針對 PokeAPI 不進行快取，保持資料最新
+  if (event.request.url.includes('pokeapi.co')) {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 清理舊版快取
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
